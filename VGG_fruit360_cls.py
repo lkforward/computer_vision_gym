083af6a423fddd35b6e1e3061fc51a4e7d70708b
Ke <lkforward@gmail.com>
2020-08-10 17:23:56 -0400

    Add files via upload

---
+++ VGG_fruit360_cls.py
@@ -0,0 +1,12 @@
+# This file is to create a (multi-class) classifier to identify 
+# fruits in the fruit360 dataset. 
+# 
+# We will use: 
+#   A pretrained VGG model; 
+#   Transfer learning to train the classifier (following the VGG
+# model). 
+# Keras in tensorflow 2.0 will be used as the library for deep 
+# learning.   
+# 
+# kliu14, created on 08/10/2020. 
+
