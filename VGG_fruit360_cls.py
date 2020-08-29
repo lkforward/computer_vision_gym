61157c7febbbce6e0f41b9382f6fab3a7827a967
Ke <lkforward@gmail.com>
2020-08-29 16:49:16 -0400

    Add code to read and show data

--- VGG_fruit360_cls.py
+++ VGG_fruit360_cls.py
@@ -14,35 +14,70 @@
 # fill modules in one by one！
 
 
-
 # 1. Data： 
 # Select three arbitrary fruits, banana, raspberry &strawberry, to 
 # demonstrate it is working. 
-# Expected output: 
-#  
+# The data generator in keras: 
+# Expected output (or how to unit test data section): 
+
+# Reference: lecture video on udemy. 
+from glob import glob
+import matplotlib.pyplot as plt
+import numpy as np
+# import PIL
+
+from tensorflow.keras.preprocessing.image import load_img
+
+# a. Get the images for visualization
+train_path = 'E:\\Coursera\\cv_udemy\\data\\fruits-360\\Training'
+test_path = 'E:\\Coursera\\cv_udemy\\data\\fruits-360\\Test'
+
+train_images = glob(train_path + '\*\*.jp*g')
+test_images = glob(test_path + '\*\*.jp*g')
+
+print("Size of all the training images:", len(train_images))
+print("Size of all the test images:", len(test_images))
+
+sample_img = np.random.choice(train_images)
+print(sample_img)
+plt.imshow(load_img(sample_img))
+plt.show()
+sample_img = np.random.choice(test_images)
+plt.imshow(load_img(sample_img))
+plt.show()
+# plt.imshow(load_img(np.random.choice(test_images)))
+
+# b. Get a generator for the model training
+
 
 # 2. Load the pretrained VGG model
 # Reference: https://www.tensorflow.org/api_docs/python/tf/keras/applications/VGG16. 
 from tensorflow import keras
 from tensorflow.keras import layers
-from tensorflow.keras.applications import VGG16
+
 from tensorflow.keras.utils import plot_model
 
 # TODO: specify input dimension!
-vgg = VGG16(include_top = False, weights='imagenet')
-vgg.summary()
+def create_model():
+    from tensorflow.keras.applications import VGG16
 
-# 3. Define my model with VGG as a feature transformer
-# VGG16 + dense
-# NOTE: Using functional APIs
-x = layers.Flatten()(vgg.output)
-output = layers.Dense(units = 3, activation='softmax')(x)
+    vgg = VGG16(include_top = False, weights='imagenet')
+    vgg.summary()
 
-model = keras.Model(inputs = vgg.input, outputs = output)
-plot_model(model)
+    # 3. Define my model with VGG as a feature transformer
+    # VGG16 + dense
+    # NOTE: Using functional APIs
+    x = layers.Flatten()(vgg.output)
+    output = layers.Dense(units = 3, activation='softmax')(x)
 
+    model = keras.Model(inputs = vgg.input, outputs = output)
+    return model
+
+# model = create_model()
+# plot_model(model)
 
 
 # 4. Model training
+
 
 # 5. View loss & accuracy
